6212aeec30c08ae2dea87a39c30943a24220ad0f
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-04 19:04:40 +0545

    WEBSCRAPING

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const request = require('request');
 const cheerio = require('cheerio');
 const { error } = require('console');
 
-request('https://puginarug.com', (error, response, html) => {
+request('https://www.saralb.com.np/', (error, response, html) => {
     if(!error && response.statusCode ==200) {
       const $ =  cheerio.load(html);
 
-      const timerFrame = $('.timer frame');
-      console.log(timerFrame);
+    //   const inner = $('.inner');
+    //   console.log(inner.html());
+
+    // const output = inner.children('span').next().text();
+    // console.log(output);
     }
 });
